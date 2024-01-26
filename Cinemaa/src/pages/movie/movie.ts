b1467979c3cc9b9e12877dcfd6d509a8269f61a9
Bastien-Lienhard <156176236+Bastien-Lienhard@users.noreply.github.com>
2024-01-26 14:38:01 +0100

    Add files via upload

---
+++ Cinemaa/src/pages/movie/movie.ts
@@ -0,0 +1,34 @@
+interface MovieDetails {
+    title: string;
+    poster_path: string;
+    overview: string;
+    id: number;
+  }
+  
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const movieId = urlParams.get('id');
+  
+  if (movieId) {
+    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=cbeed44a15dca57acd384f840eb18260`;
+  
+    fetch(url)
+      .then(response => response.json())
+      .then((movie: MovieDetails) => {
+        const movieDetailsHTML = `
+          <h2>${movie.title}</h2>
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+          <p>${movie.overview}</p>
+        `;
+  
+        const movieDetails = document.getElementById('movie-details');
+        if (movieDetails) {
+          movieDetails.innerHTML = movieDetailsHTML;
+        } else {
+          console.error('Element with id "movie-details" not found');
+        }
+      })
+      .catch(error => console.error(error));
+  } else {
+    console.error('No movie id found in the URL');
+  }
