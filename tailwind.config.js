b0ea806b6ddb0b7b4263b1afd5f86c49dfe44d8c
Louvel-Marvin <155963514+Louvel-Marvin@users.noreply.github.com>
2024-01-26 14:02:43 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [    './src/**/*.html',
+  './src/**/*.vue',
+  './src/**/*.jsx',
+  './src/**/*.js',
+  './index.html',
+  './App.vue',
+  './main.js',
+  './src/**/*.ts',
+  './src/**/*.tsx',],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
